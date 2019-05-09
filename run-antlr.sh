748c1957e013c41a779816a1a4b14364e1ce7faf
dhowe <daniel@rednoise.org>
2019-05-09 18:44:09 -0400

    log output

--- run-antlr.sh
+++ run-antlr.sh
@@ -8,17 +8,16 @@ ANTLR_JAR=/usr/local/lib/antlr-4.7.2-complete.jar
 CLASSPATH=$ANTLR_JAR:.
 
 echo
-echo Parsing $GRAMMAR.g4
+echo Cleaning...
 
 # cleanup
-rm *.java  2> /dev/null
-rm *.class  2> /dev/null
-rm *.interp  2> /dev/null
-rm *.tokens  2> /dev/null
+rm *.java *.class *.interp *.tokens 2> /dev/null
 
 if [[ $1 == "-c" ]]; then
   exit
 fi
+
+echo Parsing $GRAMMAR.g4
 
 # generate java
 java -jar $ANTLR_JAR "./$GRAMMAR.g4"
@@ -27,8 +26,11 @@ if [[ $1 == "-a" ]]; then
   exit
 fi
 
+echo Compiling...
 # compile java
 javac -classpath $CLASSPATH:. *.java
+
+echo Running $INPUT_FILE
 
 # run grammar
 grun $GRAMMAR $START_RULE -tokens -tree $INPUT_FILE | sed -e $'s/(chat/\\\n(chat/g' | sed -e $'s/(line/\\\n  (line/g'
