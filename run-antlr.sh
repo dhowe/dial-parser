9973a09210ee0246b27bef8be48befeb1ad09e30
dhowe <daniel@rednoise.org>
2019-05-09 19:30:06 -0400

    testRig

--- run-antlr.sh
+++ run-antlr.sh
@@ -5,7 +5,7 @@ START_RULE=chat
 INPUT_FILE=input.txt
 
 # not yet used
-OUTPUT_DIR=ta# not yet usedrget/generated-sources/antlr4
+OUTPUT_DIR=target/generated-sources/antlr4 # not yet used
 
 ANTLR_JAR=/usr/local/lib/antlr-4.7.2-complete.jar
 CLASSPATH=$ANTLR_JAR:.
@@ -24,6 +24,8 @@ echo Parsing $GRAMMAR.g4
 
 # generate java
 java -jar $ANTLR_JAR "./$GRAMMAR.g4"
+
+# -o target/generated-sources/antlr4 -listener -no-visitor -encoding UTF-8
 
 if [[ $1 == "-a" ]]; then
   exit
---
+++ src/AntlrTestRig.java
@@ -0,0 +1,83 @@
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import javax.print.PrintException;
+
+import org.antlr.v4.gui.TestRig;
+import org.antlr.v4.gui.Trees;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.atn.PredictionMode;
+
+public class AntlrTestRig extends TestRig
+{
+  public static void main(String[] args) throws Exception
+  {
+    TestRig testRig = new AntlrTestRig(new String[] {
+        "Dial", "chat", "-tokens", "-tree", "input.txt"
+    });
+    testRig.process();
+  }
+
+  public AntlrTestRig(String[] arg0) throws Exception
+  {
+    super(arg0);
+    // TODO Auto-generated constructor stub
+  }
+
+  protected void process(Lexer lexer, Class<? extends Parser> parserClass, Parser parser, CharStream input) throws IOException, IllegalAccessException, InvocationTargetException, PrintException
+  {
+    lexer.setInputStream(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+    tokens.fill();
+
+    if (showTokens) {
+      for (Token tok : tokens.getTokens()) {
+        if (tok instanceof CommonToken) {
+          System.out.println(((CommonToken) tok).toString(lexer));
+        }
+        else {
+          System.out.println(tok.toString());
+        }
+      }
+    }
+
+    if (startRuleName.equals(LEXER_START_RULE_NAME)) return;
+
+    if (diagnostics) {
+      parser.addErrorListener(new DiagnosticErrorListener());
+      parser.getInterpreter().setPredictionMode(PredictionMode.LL_EXACT_AMBIG_DETECTION);
+    }
+
+    if (printTree || gui || psFile != null) {
+      parser.setBuildParseTree(true);
+    }
+
+    if (SLL) { // overrides diagnostics
+      parser.getInterpreter().setPredictionMode(PredictionMode.SLL);
+    }
+
+    parser.setTokenStream(tokens);
+    parser.setTrace(trace);
+
+    try {
+      Method startRule = parserClass.getMethod(startRuleName);
+      ParserRuleContext tree = (ParserRuleContext) startRule.invoke(parser, (Object[]) null);
+
+      if (printTree) {
+        String s = tree.toStringTree(parser);
+        s = s.replaceAll("\\(line","\n  (line");
+        System.out.println("\n"+s);
+      }
+      if (gui) {
+        Trees.inspect(tree, parser);
+      }
+      if (psFile != null) {
+        Trees.save(tree, parser, psFile); // Generate postscript
+      }
+    } catch (NoSuchMethodException nsme) {
+      System.err.println("No method for rule " + startRuleName + " or it has arguments");
+    }
+  }
+}
